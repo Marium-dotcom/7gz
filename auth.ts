78781c48f39f35effc295136c30e3c3e106d90a1
Marium <marium.magdy98@gmail.com>
2026-04-18 04:08:32 +0200

    auth to mongo

--- auth.ts
+++ auth.ts
@@ -2,8 +2,21 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import { connectToDB } from "./libs/mongoose"
- 
+import User from "./libs/models/user"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-//   send data to the database whenever a user signs in
+  callbacks: {
+    async signIn({ user }) {
+      await connectToDB()
+      const existing = await User.findOne({ email: user.email })
+      if (!existing) {
+        await User.create({
+          name: user.name,
+          email: user.email,
+        })
+      }
+      return true
+    },
+  },
 })
